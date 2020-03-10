6277f3f2ae9c7138daf340f37ee337288636b2ce
alex <art@mail.ru>
2020-03-10 21:23:09 +0300

    first code

---
+++ json/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace json
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            User art = new User();
+            User bart = new User();
+
+            art.Name = "SETTER";
+            art.Sourname = "Jenkins";
+
+            bart.Name = "Ftidda";
+            bart.Sourname = "Kokins";
+
+            string json1 = JsonConvert.SerializeObject(art);
+            MessageBox.Show(json1);
+        }
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+        public string Sourname { get; set; }
+    }
+}
